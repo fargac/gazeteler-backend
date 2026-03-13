28e61f9da28cefd089fbb43ca6ce6796dcb8fc91
fargac <fargac@hotmail.com>
2026-03-13 08:35:01 +0300

    hurriyet 15 e çekildi.

--- data/haber_kaynaklari.py
+++ data/haber_kaynaklari.py
@@ -97,7 +97,7 @@ NEWS_SOURCES = [
         "id": "hurriyet",
         "name": "Hürriyet",
         "rss": "https://www.hurriyet.com.tr/rss/anasayfa",
-        "maxItems": 51,
+        "maxItems": 15,
         "excludeCategories": ["Resmi İlanlar", "Advertorial","Lezizz","Kelebek","Spor Arena","Aile"],
         "rules": {
             "item": RULE_ITEM_RSS,
